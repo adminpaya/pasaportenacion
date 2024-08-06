c665137946db15960d82ed141a56cf9c3136c5a4
adminpaya <114530140+adminpaya@users.noreply.github.com>
2024-08-06 15:04:55 -0300

    Update msg_send.php

--- msg_send.php
+++ msg_send.php
@@ -1,26 +1,52 @@
-<?php
-
-$curl = curl_init();
-
-curl_setopt_array($curl, array(
-  CURLOPT_URL => 'https://bot.pidepaya.com/pasaporte',
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'POST',
-  CURLOPT_POSTFIELDS =>'{
-    "Phone": "50766183087",
-    "Message": "cors avoidance test"
-}',
-  CURLOPT_HTTPHEADER => array(
-    'Content-Type: application/json'
-  ),
-));
-
-$response = curl_exec($curl);
-
-curl_close($curl);
-echo $response;
+<?php
+
+
+
+$curl = curl_init();
+
+
+
+curl_setopt_array($curl, array(
+
+  CURLOPT_URL => 'https://whatsappmsgendpoint',
+
+  CURLOPT_RETURNTRANSFER => true,
+
+  CURLOPT_ENCODING => '',
+
+  CURLOPT_MAXREDIRS => 10,
+
+  CURLOPT_TIMEOUT => 0,
+
+  CURLOPT_FOLLOWLOCATION => true,
+
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+
+  CURLOPT_CUSTOMREQUEST => 'POST',
+
+  CURLOPT_POSTFIELDS =>'{
+
+    "Phone": "phone number",
+
+    "Message": "cors avoidance test"
+
+}',
+
+  CURLOPT_HTTPHEADER => array(
+
+    'Content-Type: application/json'
+
+  ),
+
+));
+
+
+
+$response = curl_exec($curl);
+
+
+
+curl_close($curl);
+
+echo $response;
+
